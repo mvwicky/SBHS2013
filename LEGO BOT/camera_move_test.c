242cdb4299a141f639bf1e2fb211383c289ef4b2
Michael Van Wickle <mvanwickle@gmail.com>
2013-04-27 13:08:24 -0700

    updates to camera move

--- LEGO BOT/camera_move_test.c
+++ LEGO BOT/camera_move_test.c
@@ -6,7 +6,7 @@
 #define ARM_DOWN 1800 // down for the pom getter arm
 #define B_UP 1600 // up for the basket
 #define B_DOWN 750 // down for the basket (dumping)
-#define TOL 5
+#define TOL 1
 
 inline int camera_move_x();
 inline int camera_move_y();
@@ -42,16 +42,24 @@ int main()
 	while(a_button() == 0);
 	while (1) // position to get the pom
 	{
-		camera_move_y();
-		camera_move_x();
-		x_in = (target.x >= (target.x - TOL) && (target.x <= target.x + TOL));
+		camera_update();
+		current.x = get_object_center(0 , 0).x;
+		current.y = get_object_center(0 , 0).y;
+		printf("(%d , %d) , (%d , %d)\n" , target.x , target.y , current.x , current.y);
+		x_in = (current.x >= (target.x - TOL) && (current.x <= target.x + TOL));
 		// is true if the x coordinate is equal to the target or within a ten unit range (5 on each side)
-		y_in = (target.y >= (target.y - TOL) && (target.y <= target.y + TOL));
+		y_in = (current.y >= (target.y - TOL) && (current.y <= target.y + TOL));
 		// is true if the y coordinate is equal to the target or within a ten unit range (5 on each side)
-		if (y_in == true && x_in == true)
+		/*
+		if ((current.y >= (target.y - TOL) && (current.y <= target.y + TOL)) && (current.x >= (target.x - TOL) && (current.x <= target.x + TOL)))
 		{
+			printf("IN POS\n");
 			break;
 		}
+		camera_move_y();
+		camera_move_x();
+		*/
+		
 	}	
 	set_servo_position(arm_servo , ARM_DOWN);
 	msleep(500);
@@ -74,7 +82,7 @@ inline int camera_move_x()
 		printf("LEFT\n");
 		mav(lego.left.port , lspeed);
 		mav(lego.right.port , hspeed);
-		msleep(1);
+		msleep(10);
 		return 1;
 	}
 	if (get_object_center(0 , 0).x > target.x)
@@ -82,7 +90,7 @@ inline int camera_move_x()
 		printf("RIGHT\n");
 		mav(lego.left.port , hspeed);
 		mav(lego.right.port , lspeed);
-		msleep(1);
+		msleep(10);
 		return 1;
 	}
 	if (get_object_center(0 , 0).x >= target.x && get_object_center(0 , 0).x <= target.x)
@@ -104,7 +112,7 @@ inline int camera_move_y()
 		printf("TOO CLOSE\n");
 		mav(lego.left.port , back);
 		mav(lego.right.port , back);
-		msleep(1);
+		msleep(10);
 		return 1;
 	}
 	if (get_object_center(0 , 0).y < target.y)
@@ -112,7 +120,7 @@ inline int camera_move_y()
 		printf("TOO FAR\n");
 		mav(lego.left.port , speed);
 		mav(lego.left.port , speed);
-		msleep(1);
+		msleep(10);
 		return 1;
 	}
 	if (get_object_center(0 , 0).y >= target.y && get_object_center(0 , 0).y <= target.y)
